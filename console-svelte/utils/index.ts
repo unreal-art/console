b9bf0fd4b1ae383569828950b6359109311ff14f
Nasfame <laciferin2024@protonmail.com>
2025-07-05 04:32:37 +0400

    wip:

--- console-svelte/utils/index.ts
+++ console-svelte/utils/index.ts
@@ -1,5 +1,2 @@
-export * as deepLinking from "./deepLinking";
-export * as getBrowser from "./getBrowser";
-export * as reclaim from "./reclaim";
-export * as unshorten from "./unshorten";
 export * as isWebpageExtension from "./isWebpageExtension";
+export * as getBrowser from "./getBrowser";
