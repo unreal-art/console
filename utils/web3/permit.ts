fd8dff4b0d15d472d8eb94f5a858b44e6269ea6a
Nasfame <laciferin2024@protonmail.com>
2025-08-31 03:48:21 +0530

    remove: linkts and fix

--- utils/web3/permit.ts
+++ utils/web3/permit.ts
@@ -1,1 +1,74 @@
-/Volumes/N/Unreal/openai/src/web3/permit.ts
+limport { getPublicClient } from "@config/wallet"
+import {
+  Account,
+  Address,
+  PublicClient,
+  Signature,
+  TransactionReceipt,
+  WalletClient,
+  parseSignature,
+} from "viem"
+import { waitForTransactionReceipt } from "./torus"
+
+// ERC20 Permit ABI (EIP-2612)
+const erc20PermitAbi = [
+  {
+    inputs: [
+      { internalType: "address", name: "owner", type: "address" },
+      { internalType: "address", name: "spender", type: "address" },
+      { internalType: "uint256", name: "value", type: "uint256" },
+      { internalType: "uint256", name: "deadline", type: "uint256" },
+      { internalType: "uint8", name: "v", type: "uint8" },
+      { internalType: "bytes32", name: "r", type: "bytes32" },
+      { internalType: "bytes32", name: "s", type: "bytes32" },
+    ],
+    name: "permit",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+]
+
+export interface PermitData {
+  owner: Address
+  spender: Address
+  value: bigint
+  nonce: bigint
+  deadline: bigint
+}
+
+export async function executePermit(
+  token: Address,
+  permit: PermitData,
+  permitSignature: `0x${string}`,
+  wallet: WalletClient,
+  publicClient: PublicClient,
+  confirmationWait?: number
+): Promise<{ txHash: string; cb: Promise<TransactionReceipt> }> {
+  const { r, s, v, yParity } = parseSignature(permitSignature)
+
+  console.log(
+    JSON.stringify({
+      owner: permit.owner,
+      spender: permit.spender,
+      nonce: permit.nonce.toString(),
+      value: permit.value.toString(),
+      deadline: permit.deadline.toString(),
+    })
+  )
+
+  const txHash = await wallet.writeContract({
+    address: token,
+    abi: erc20PermitAbi,
+    functionName: "permit",
+    args: [permit.owner, permit.spender, permit.value, permit.deadline, v, r, s],
+    account: wallet.account,
+  })
+
+  console.log("permit Tx", txHash)
+  // console.log("permit", permit)
+
+  const cb = waitForTransactionReceipt(publicClient, txHash, 1, confirmationWait) //FIXME: wallet.account.client might not be configrued
+  // await cb
+  return { txHash, cb }
+}
